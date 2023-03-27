596ed5c9df668cfbee888263da81c325382431f6
GGos3 <janpll0421@gmail.com>
2023-03-27 19:10:02 +0900

    INIT: Spring 프로젝트 생성

---
+++ src/main/java/fx/backend/FXBackendApplication.java
@@ -0,0 +1,13 @@
+package fx.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FXBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FXBackendApplication.class, args);
+	}
+
+}
