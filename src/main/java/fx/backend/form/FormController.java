1b9121e2e5d383029f783907ffa8e0ecf0fee7bf
GGos3 <janpll0421@gmail.com>
2023-03-28 17:35:25 +0900

    FEAT: FormController 추가

---
+++ src/main/java/fx/backend/form/FormController.java
@@ -0,0 +1,26 @@
+package fx.backend.form;
+
+import fx.backend.domain.Order;
+import fx.backend.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class FormController {
+    private final OrderService orderService;
+
+    @PostMapping("/form/save")
+    public String formSave(@RequestBody Order order) {
+        orderService.save(order);
+
+        return "ok";
+    }
+
+}
