a227010686c6b9601b8431b94f2a911425d09f7a
GGos3 <janpll0421@gmail.com>
2023-03-29 01:09:02 +0900

    FEAT: /admin/edit 페이지 매핑

--- src/main/java/fx/backend/admin/AdminController.java
+++ src/main/java/fx/backend/admin/AdminController.java
@@ -1,12 +1,26 @@
 package fx.backend.admin;
 
+import fx.backend.domain.Order;
+import fx.backend.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final OrderService orderService;
+
+    @PatchMapping("/edit")
+    public HttpStatus editOrder(Long orderId, @ModelAttribute Order order) {
+
+        return HttpStatus.OK;
+    }
 }
