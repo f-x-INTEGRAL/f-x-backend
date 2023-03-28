61826a4d6fc82b8bd5c99a28a5613f6fe7dbdfc0
GGos3 <janpll0421@gmail.com>
2023-03-28 17:35:09 +0900

    FEAT: AdminController 추가

---
+++ src/main/java/fx/backend/admin/AdminController.java
@@ -0,0 +1,12 @@
+package fx.backend.admin;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+}
