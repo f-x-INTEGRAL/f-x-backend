1fe9e124e6317528c21fd71a876d78402263850b
GGos3 <janpll0421@gmail.com>
2023-03-29 12:12:52 +0900

    FEAT: /form/save 페이지 처리

--- src/main/java/fx/backend/form/FormController.java
+++ src/main/java/fx/backend/form/FormController.java
@@ -4,10 +4,10 @@ import fx.backend.domain.Order;
 import fx.backend.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @RestController
@@ -15,13 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class FormController {
     private final OrderService orderService;
-
     @PostMapping("/form/save")
-    public String formSave(@RequestBody Order order) {
-        orderService.save(order);
+    public Map<Order, String> formSave(@ModelAttribute Order order) {
+        Map<Order, String> respData = new ConcurrentHashMap<>();
+
+        try {
+            orderService.save(order);
+        } catch (IllegalArgumentException e) {
+            respData.put(order, e.getMessage());
+            return respData;
+        }
         log.info("order={}", order);
+        respData.put(order, "정상적으로 처리 되었습니다.");
 
-        return "ok";
+        return respData;
     }
-
 }
