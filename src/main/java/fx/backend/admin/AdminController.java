d7744cfc4c7aa16c10093c493de780d34101193a
GGos3 <janpll0421@gmail.com>
2023-03-31 12:10:42 +0900

    FEAT: 모든 Order 조회 기능 추가

--- src/main/java/fx/backend/admin/AdminController.java
+++ src/main/java/fx/backend/admin/AdminController.java
@@ -4,11 +4,12 @@ import fx.backend.domain.Order;
 import fx.backend.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.RequestEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -22,5 +23,16 @@ public class AdminController {
     public HttpStatus editOrder(Long orderId, @ModelAttribute Order order) {
 
         return HttpStatus.OK;
+    }
+
+    /**
+     * 요청이 들어오면 모든 order 를 반환함
+     *
+     * @return
+     */
+
+    @PostMapping("/find-all")
+    public List<Order> findAllOrder() {
+        return orderService.findAll();
     }
 }
