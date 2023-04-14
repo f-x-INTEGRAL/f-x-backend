439bb5a129c20fae3c017a4e7b00a53d7be30414
GGos3 <janpll0421@gmail.com>
2023-04-14 15:38:54 +0900

    REFACTOR: 변수명 변경

--- src/main/java/fx/backend/domain/Order.java
+++ src/main/java/fx/backend/domain/Order.java
@@ -1,20 +1,1 @@
-package fx.backend.domain;
-
-import lombok.Data;
-
-@Data
-public class Order {
-    private Long ID;
-    private String name;
-    private Integer headCount;
-    private String phoneNumber;
-    private String bankName;
-    private OrderStatus status = OrderStatus.WAITING;
-
-    public Order(String name, Integer headCount, String phoneNumber, String bankName) {
-        this.name = name;
-        this.headCount = headCount;
-        this.phoneNumber = phoneNumber;
-        this.bankName = bankName;
-    }
-}
+package fx.backend.domain;import lombok.Data;@Datapublic class Order {    private Long ID;    private String name;    private Integer quantity;    private String phoneNumber;    private String bankName;    private OrderStatus status = OrderStatus.WAITING;    public Order(String name, Integer quantity, String phoneNumber, String bankName) {        this.name = name;        this.quantity = quantity;        this.phoneNumber = phoneNumber;        this.bankName = bankName;    }}
