20f0cd788c7c4eae211a7a5085df9b0a9fb62e66
GGos3 <janpll0421@gmail.com>
2023-04-15 22:32:09 +0900

    REFACTOR: Service 반환타입 변경 및 예외 추가

--- src/main/java/fx/backend/service/OrderService.java
+++ src/main/java/fx/backend/service/OrderService.java
@@ -1,45 +1,60 @@
 package fx.backend.service;
 
-import fx.backend.domain.Order;
+import fx.backend.domain.Orders;
 import fx.backend.repository.OrderRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    public void save(Order order) {
-        validateDuplicatePhoneNumber(order);
-        orderRepository.save(order);
+    public void save(Orders orders) {
+        validateDuplicatePhoneNumber(orders);
+        orderRepository.save(orders);
     }
 
-    private void validateDuplicatePhoneNumber(Order order) {
-        orderRepository.findByPhoneNumber(order.getPhoneNumber()).ifPresent(a -> {
+    private void validateDuplicatePhoneNumber(Orders orders) {
+        orderRepository.findByPhoneNumber(orders.getPhoneNumber()).ifPresent(a -> {
             throw new IllegalArgumentException("이미 존재하는 전화번호 입니다.");
         });
     }
 
-    public Optional<Order> findByPhoneNumber(String phoneNumber) {
+    public Optional<Orders> findByPhoneNumber(String phoneNumber) {
         return orderRepository.findByPhoneNumber(phoneNumber);
     }
 
-    public Optional<Long> deleteOrder(Long orderId) {
-        orderRepository.deleteOrder(orderId);
+    public Orders updateOrder(Long orderId, Orders order) {
+        Orders orders = findById(orderId);
 
-        return Optional.ofNullable(orderId);
+        orders.setStatus(order.getStatus());
+        save(orders);
+
+        return orders;
     }
 
-    public List<Order> findAll() {
-        return orderRepository.findAlL();
+    public void deleteOrder(Long orderId) {
+        orderRepository.deleteById(orderId);
     }
 
-    public Optional<Order> findById(Long Id){
-        return orderRepository.findById(Id);
+    public List<Orders> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public Orders findById(Long Id){
+        Optional<Orders> orders = orderRepository.findById(Id);
+
+        if (orders.isEmpty())
+            throw new NoSuchElementException(Id + "번 아이템은 존재하지 않습니다.");
+
+        return orders.get();
     }
 }
