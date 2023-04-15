1aa18314e50f19a60071c15e87993650342bb71d
GGos3 <janpll0421@gmail.com>
2023-04-15 22:33:22 +0900

    REFACTOR: 반환 타입 변경 및 불필요한 메서드 제거

--- src/main/java/fx/backend/repository/OrderRepository.java
+++ src/main/java/fx/backend/repository/OrderRepository.java
@@ -1,16 +1,14 @@
 package fx.backend.repository;
 
-import fx.backend.domain.Order;
+import fx.backend.domain.Orders;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository {
-    Order save(Order order);
-    Optional<Order> findById(Long orderId);
-    Optional<Order> findByPhoneNumber(String phoneNumber);
-    List<Order> findAlL();
-    Optional<Long> deleteOrder(Long orderId);
-    Optional<Order> editOrder(Long orderId, Order order);
-    void clearAll();
+    Orders save(Orders orders);
+    Optional<Orders> findById(Long orderId);
+    Optional<Orders> findByPhoneNumber(String phoneNumber);
+    List<Orders> findAll();
+   void deleteById(Long orderId);
 }
