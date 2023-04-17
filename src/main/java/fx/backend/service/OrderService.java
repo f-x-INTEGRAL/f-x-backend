de61f9544a5c2435e142749a9118e29895d58af9
GGos3 <janpll0421@gmail.com>
2023-04-18 00:05:33 +0900

    FEAT: 환경 변수를 통한 로그인 기능 추가

--- src/main/java/fx/backend/service/OrderService.java
+++ src/main/java/fx/backend/service/OrderService.java
@@ -5,16 +5,23 @@ import fx.backend.domain.Orders;
 import fx.backend.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
+@PropertySource("classpath:application.properties")
 public class OrderService {
+    @Value("${admin.password}")
+    private String password;
 
     private final OrderRepository orderRepository;
 
@@ -57,5 +64,11 @@ public class OrderService {
             throw new NoSuchElementException(Id + "번 아이템은 존재하지 않습니다.");
 
         return orders.get();
+    }
+
+    public boolean login(String userPassword) {
+        log.info("password={}", password);
+        log.info("userPassword={}", userPassword);
+        return userPassword.equals(password);
     }
 }
