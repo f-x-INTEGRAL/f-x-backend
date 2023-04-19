da69df840fd2e4011a6c4060e45761921300330f
GGos3 <janpll0421@gmail.com>
2023-04-19 11:14:38 +0900

    FEAT: 티켓 수량 검증로직 추가

--- src/main/java/fx/backend/repository/SpringDataJpaRepository.java
+++ src/main/java/fx/backend/repository/SpringDataJpaRepository.java
@@ -1,9 +1,13 @@
 package fx.backend.repository;
 
+import fx.backend.domain.OrderStatus;
 import fx.backend.domain.Orders;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 @Primary
 public interface SpringDataJpaRepository extends JpaRepository<Orders, Long>, OrderRepository{
+    @Query("SELECT SUM(o.quantity) FROM Orders o WHERE o.status = 1")
+    Long sumConformedOrdersQuantity();
 }
